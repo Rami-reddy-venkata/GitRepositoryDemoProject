e88450f7a9fda5af20e9612f055b032d2344029e
sys <sys@user>
2021-07-27 19:50:19 +0530

    commiting test2 to MyLocalBranch

---
+++ src/com/mindtree/project/Test2.java
@@ -0,0 +1,11 @@
+package com.mindtree.project;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello world");
+        System.out.println("Modifired");
+	}
+
+}
